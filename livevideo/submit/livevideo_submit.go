20918fe99fc8c0b47746a8010b8e0e17a112fad4
sunlifang <sunlifang@corp.netease.com>
2022-06-12 14:10:07 +0800

    直播视频接口升级至v4版

--- livevideo/submit/livevideo_submit.go
+++ livevideo/submit/livevideo_submit.go
@@ -26,8 +26,8 @@ import (
 )
 
 const (
-	apiUrl     = "http://as.dun.163.com/v3/livevideo/submit"
-	version    = "v3.1"
+	apiUrl     = "http://as.dun.163.com/v4/livevideo/submit"
+	version    = "v4"
 	secretId   = "your_secret_id"   //产品密钥ID，产品标识
 	secretKey  = "your_secret_key"  //产品私有密钥，服务端生成签名信息使用，请严格保管，避免泄露
 	businessId = "your_business_id" //业务ID，易盾根据产品业务特点分配
